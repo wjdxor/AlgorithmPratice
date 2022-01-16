4ca657ebfbaf244a43e3a45974e29235b05da413
LIM JT(임정택) <38105420+wjdxor@users.noreply.github.com>
2022-01-17 06:03:52 +0900

    add

---
+++ api_recept.py
@@ -0,0 +1,4 @@
+import requests, xmltodict, json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
--- test.py
+++ test.py
@@ -1,33 +1,11 @@
-# def solution(lottos, win_nums):
-#     answer = []
-#     count = 0
-#     count2 = 0
-
-#     for i in range(6):
-#         if lottos[i] == 0:
-#             count2 += 1
-#             continue
-#         else:
-#             for j in range(6):
-#                 if lottos[i] == win_nums[j]:
-#                     count += 1
-#     #최저 순위
-#     rank = 7 - count
-#     #최고 가능 순위
-#     rank_able = rank - count2
-
-#     if rank == 7:
-#         rank == 6
-#     if rank_able == 7:
-#         rank_able == 6
-#     answer = [rank_able,rank]
-#     return answer
-
-
-
-# print(solution([1, 2, 3, 4, 0, 0], [10, 11, 12, 13, 14, 15]))
-
-s = 'rkskekfkfk'
-a = list(s)
-for i in range(5):
-    print("1")
+import requests 
+import json 
+# GET 
+res = requests.get('http://apis.data.go.kr/3120000/bizUtil/data?serviceKey=OfJRD4FTDToxwYcjmf7xMNJ6v07sE3IhZxn2xGB0t1tLMrHv%2FP8mbPcCln409zA3kTQB7KZ84TsbStxaa%2B6KuA%3D%3D&numOfRows=10&pageNo=1&date=20210101&time=1')
+print(str(res.status_code) + " | " + res.text) 
+# POST (JSON) 
+headers = {'Content-Type': 'application/json; chearset=utf-8'} 
+data = {'title': 'dummy title', 'id': 1, 'message': 'hello world!'} 
+res = requests.post('http://127.0.0.1:5000', 
+data=json.dumps(data), headers=headers) 
+print(str(res.status_code) + " | " + res.text)
